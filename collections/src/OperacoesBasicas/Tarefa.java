5d6ccca82b046776ccbe429186ef83babf64c582
VictorHugo7 <victorhugoaraujo1999@gmail.com>
2025-05-07 02:25:26 -0300

    Resolvendo desafio ListaTarefa

---
+++ collections/src/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,21 @@
+package OperacoesBasicas;
+
+public class Tarefa {
+	
+	public Tarefa(String descricao) {
+		super();
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	private String descricao;
+	
+	
+}
